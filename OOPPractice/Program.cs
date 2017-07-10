c8e99be462e4a778bbccbf9a3575b1148db799f2
pfittante <pfittante@yahoo.com>
2017-07-10 14:23:08 -0400

    OOP Practice definitions

--- OOPPractice/Program.cs
+++ OOPPractice/Program.cs
@@ -10,6 +10,17 @@ namespace OOPPractice
     {
         static void Main(string[] args)
         {
+            //When we initialize a new object it is call instantiated and must be instantiated before it can be used
+            //All object of a specific type are created from the same template or class
+            //All objects from a class will have the same number of states but each instance can have it own unique values
+            //The format we use to instantiate and object looks like this
+            //ClassName objectname = new ClassName()
+            //ie Cat mittens = new Cat(); This is called a constructor  Cat = ClassName  mittens = objectName  same as int[] array = new int[]
+            //                          (adjectives)                                (Verbs)
+            //              object      characteristics                             behaviours
+            //              house       hasDoors hasWindows hasFloors heated        Heats() Cools() Protects()
+            //              pool        hasWater hasSides isHeated                  Cools() Wets() 
+            //              cig         hasFilter hasPaper hasTobacco               Kills() Burns() Satisfies()
         }
     }
 }
