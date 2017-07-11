56b97df0531addccd8008cf960f414b54c698e0d
pfittante <pfittante@yahoo.com>
2017-07-11 13:22:37 -0400

    Built Superhero constr prop and fields

--- OOPPractice/SuperHero.cs
+++ OOPPractice/SuperHero.cs
@@ -8,5 +8,71 @@ namespace OOPPractice
 {
     class SuperHero
     {
+
+        //fields
+        private string costume;
+        private string name;
+        private int strengthLevel;
+        private string power;
+        private bool hasCape;
+
+        //End Fields
+        //Properties
+
+        public string Costume
+        {
+            get { return this.costume; }
+            set { this.costume = value; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int StrenghtLevel
+        {
+            get { return this.strengthLevel; }
+            set { this.strengthLevel = value; }
+        }
+
+        public string Power
+        {
+            get { return this.power; }
+            set { this.power = value; }
+        }
+
+        public bool HasCape
+        {
+            get { return this.hasCape; }
+            set { this.hasCape = value; }
+        }
+        //End Properties
+        //Constructors
+
+        public SuperHero()
+        {
+            //Default constructor
+        }
+
+        public SuperHero(string costumeSH, string nameSH, int strengthLevelSH, string powerSH, bool hasCapeSH)
+        {
+            this.costume = costumeSH;
+            this.name = nameSH;
+            this.strengthLevel = strengthLevelSH;
+            this.power = powerSH;
+            this.hasCape = hasCapeSH;
+        }
+        public SuperHero(string nameSH, int strengthLevelSH)
+        {
+            this.name = nameSH;
+            this.strengthLevel = 100;
+        }
+        //End Constructors
+
+        //Methods
+
+
     }
 }
